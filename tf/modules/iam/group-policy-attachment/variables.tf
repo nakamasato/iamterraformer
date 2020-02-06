b213aced03bf0da72cfd96b32fe0f4d6c880e8b9
Naka Masato <masatonaka1989@gmail.com>
2020-02-07 00:06:59 +0900

    Simple version (#10)
    * change uml
    
    * [simple version] fix convert.sh
    
    * remove modules

--- tf/modules/iam/group-policy-attachment/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "group_policy_pairs" {
-  description = "list of pairs of group and policy with 'group_name' and 'policy_arns' keys, used for iam_group_policy_attachment"
-  type = set(object({
-    group_name = string
-    policy_arn = string
-    name       = string
-  }))
-}
